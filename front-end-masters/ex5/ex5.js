f3931ec6d1e5228aa136175cd2cefe739337ecef
maighdlyn <emmamaighdlyn@gmail.com>
2017-06-08 17:42:46 -0700

    Completes Frontend Masters exercise 5

--- front-end-masters/ex5/ex5.js
+++ front-end-masters/ex5/ex5.js
@@ -1,27 +1,38 @@
-function fakeAjax(url,cb) {
-	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
-	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+function fakeAjax (url, cb) {
+  var fakeResponses = {
+    'file1': 'The first text',
+    'file2': 'The middle text',
+    'file3': 'The last text'
+  }
+  var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000
 
-	console.log("Requesting: " + url);
+  console.log('Requesting: ' + url)
 
-	setTimeout(function(){
-		cb(fake_responses[url]);
-	},randomDelay);
+  setTimeout(function () {
+    cb(fakeResponses[url])
+  }, randomDelay)
 }
 
-function output(text) {
-	console.log(text);
+function output (text) {
+  console.log(text)
 }
 
 // **************************************
 
-function getFile(file) {
-	// what do we do here?
+function getFile (file) {
+  return ASQ(function (done) {
+    fakeAjax(file, done)
+  })
 }
 
-// request an array of files at once in "parallel"
-// ???
+// request an array of files at once in 'parallel'
+
+getFile('file1')
+.val(output)
+.seq(getFile('file2'))
+.val(output)
+.seq(getFile('file3'))
+.val(output)
+.val(function () {
+  output('Complete!')
+})
