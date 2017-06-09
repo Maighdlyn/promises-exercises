0a17f2a4ed8a71e069933a27aa90f2b7d488ef33
maighdlyn <emmamaighdlyn@gmail.com>
2017-06-08 17:13:41 -0700

    Completes Frontend Masters exercise 3

--- front-end-masters/ex3/ex3.js
+++ front-end-masters/ex3/ex3.js
@@ -1,27 +1,46 @@
-function fakeAjax(url,cb) {
-	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
-	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+function fakeAjax (url, cb) {
+  var fakeResponses = {
+    'file1': 'The first text',
+    'file2': 'The middle text',
+    'file3': 'The last text'
+  }
+  var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000
 
-	console.log("Requesting: " + url);
+  console.log('Requesting: ' + url)
 
-	setTimeout(function(){
-		cb(fake_responses[url]);
-	},randomDelay);
+  setTimeout(function () {
+    cb(fakeResponses[url])
+  }, randomDelay)
 }
 
-function output(text) {
-	console.log(text);
+function output (text) {
+  console.log(text)
 }
 
 // **************************************
 
-function getFile(file) {
-	// what do we do here?
+function getFile (file) {
+  return new Promise(function executor (resolve, reject) {
+    fakeAjax(file, resolve)
+  })
 }
 
-// request all files at once in "parallel"
-// ???
+// request all files at once in 'parallel'
+
+var promise1 = getFile('file1')
+var promise2 = getFile('file2')
+var promise3 = getFile('file3')
+
+promise1
+.then(output)
+.then(function () {
+  return promise2
+})
+.then(output)
+.then(function () {
+  return promise3
+})
+.then(output)
+.then(function () {
+  output('Complete!')
+})
